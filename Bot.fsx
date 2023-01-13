f013381071a99b4deae560825985bab643b7b918
Hadrian Tang <hadrianwttang@outlook.com>
2023-01-14 00:01:55 +0800

    Add source guilds

--- Bot.fsx
+++ Bot.fsx
@@ -1,50 +1,86 @@
 #r "nuget: Ical.NET, 4.2.0"
-#r "nuget: Discord.NET, 3.6.1"
-let identifiableLocation = "International F# online event, see description"
+#r "nuget: Discord.NET, 3.9.0"
+// F#-related Discord servers that may schedule events
+let sourceGuilds = Map [
+    716980335593914419UL, "https://discord.gg/bpTJMbSSYK" // Fabulous
+    940511234179096586UL, "https://discord.gg/D5QXvQrBVa" // Fantomas
+]
 task {
     printfn "Started."
-    let calendarEvents =
-        use http = new System.Net.Http.HttpClient()
-        // https://sergeytihon.com/f-events/
-        use stream = http.GetStreamAsync "https://calendar.google.com/calendar/ical/retcpic7o1iggr3cmqio8lcu8k%40group.calendar.google.com/public/basic.ics" |> fun x -> x.Result
-        Ical.Net.Calendar.Load(stream).Events
+    use http = new System.Net.Http.HttpClient()
+    // https://sergeytihon.com/f-events/
+    use! calendarStream = http.GetStreamAsync "https://calendar.google.com/calendar/ical/retcpic7o1iggr3cmqio8lcu8k%40group.calendar.google.com/public/basic.ics"
+    let calendarEvents = Ical.Net.Calendar.Load(calendarStream).Events
     use client = new Discord.WebSocket.DiscordSocketClient()
     client.add_Log(fun msg -> task { printfn $"{msg}" })
     do! client.LoginAsync(Discord.TokenType.Bot, System.Environment.GetEnvironmentVariable "BOT_LOGIN_TOKEN")
     do! client.StartAsync()
-
+    let sourceEvents =  [
+        for KeyValue (id, invite) in sourceGuilds do
+            let sourceGuild = client.GetGuild id
+            // Don't crash if we're not in one of the source guilds
+            if sourceGuild <> null then
+                let iconStream = http.GetStreamAsync sourceGuild.IconUrl
+                // Discord API limitation: see beliow, location max length 100
+                $"{sourceGuild.Name[..99 - invite.Length - 1]} {invite}", iconStream, sourceGuild.Events
+    ]
     let completion = System.Threading.Tasks.TaskCompletionSource()
     client.add_Ready(fun () -> task {
         printfn "Ready. Processing started."
-        let utcNowOffset = System.DateTimeOffset.UtcNow
-        let utcNow = utcNowOffset.UtcDateTime
+        let now = System.DateTimeOffset.UtcNow
+        let maxEnd = now.AddYears(5).AddSeconds(-1.)
         for guild in client.Guilds do
-            let existingDiscordEvents = System.Linq.Enumerable.ToDictionary (guild.Events |> Seq.filter (fun e -> e.Location = identifiableLocation), fun e -> e.Name)
-            for e in calendarEvents do
-                if e.DtStart.AsUtc > utcNow then // Don't add new already started events
+            if Map.containsKey guild.Id sourceGuilds then () else // Ignore source guilds
+            let existingDiscordEvents = System.Linq.Enumerable.ToDictionary (guild.Events |> Seq.filter (fun e -> e.Creator.Id = client.CurrentUser.Id), fun e -> e.Location, e.Name)
+            let syncOneEvent location name startTime description endTime icon = task {
+                if now < startTime && startTime <= maxEnd && startTime <= endTime then // Discord API limitation: Don't add new already started events (including now) or start time >= 5 years into future (precise to seconds) or start time > end time (can equal)
                     try
-                        match (existingDiscordEvents.Remove: _ -> _ * _) e.Summary with
+                        // Discord API limitation: string length limits
+                        let name = (name: string)[..99]
+                        let description = (description: string)[..999]
+                        let location = (location: string)[..99]
+                        // Discord API limitation: end time < 5 years into the future (precise to seconds)
+                        let endTime = min endTime maxEnd
+                        match (existingDiscordEvents.Remove: _ -> _ * _) (location, name) with
                         | false, _ ->
-                            let! _ = guild.CreateEventAsync(e.Summary, e.DtStart.AsDateTimeOffset, Discord.GuildScheduledEventType.External,
-                                 Discord.GuildScheduledEventPrivacyLevel.Private, e.Description, e.DtEnd.AsDateTimeOffset, System.Nullable(), identifiableLocation)
-                            printfn $"Created scheduled event '{e.Summary}' for '{guild}'."
+                            printfn $"Creating '{location}' event '{name}' for '{guild}'..."
+                            let! _ = guild.CreateEventAsync(name, startTime, Discord.GuildScheduledEventType.External,
+                                Discord.GuildScheduledEventPrivacyLevel.Private, description, System.Nullable endTime, System.Nullable(), location)
+                            ()
                         | true, existingDiscordEvent ->
+                            if existingDiscordEvent.Name = name
+                                && existingDiscordEvent.StartTime = startTime
+                                && existingDiscordEvent.Type = Discord.GuildScheduledEventType.External
+                                && existingDiscordEvent.PrivacyLevel = Discord.GuildScheduledEventPrivacyLevel.Private
+                                && existingDiscordEvent.Description = description
+                                && existingDiscordEvent.EndTime.HasValue
+                                && existingDiscordEvent.EndTime.GetValueOrDefault() = endTime
+                                && existingDiscordEvent.Location = location
+                            then () else // Minimise request count
+                            printfn $"Modifing '{location}' event '{name}' for '{guild}'..."
                             do! existingDiscordEvent.ModifyAsync(fun props ->
-                                props.Name <- e.Summary
-                                props.StartTime <- e.DtStart.AsDateTimeOffset
+                                props.Name <- name
+                                props.StartTime <- startTime
                                 props.Type <- Discord.GuildScheduledEventType.External
                                 props.PrivacyLevel <- Discord.GuildScheduledEventPrivacyLevel.Private
-                                props.Description <- e.Description
-                                props.EndTime <- e.DtEnd.AsDateTimeOffset
+                                props.Description <- description
+                                props.EndTime <- endTime
                                 props.ChannelId <- Discord.Optional.Create(System.Nullable())
-                                props.Location <- identifiableLocation
+                                props.Location <- location
+                                props.CoverImage <- new Discord.Image(icon: System.IO.Stream) |> System.Nullable |> Discord.Optional
                             )
-                            printfn $"Modified scheduled event '{e.Summary}' for '{guild}'."
-                    with exn -> printfn $"Error processing scheduled event '{e.Summary}' for '{guild}'.\n{exn}"
+                    with exn -> printfn $"Error processing '{location}' event '{name}' for '{guild}'.\n{exn}"
+            }
+            for e in calendarEvents do
+                do! syncOneEvent "F# Events Calendar https://sergeytihon.com/f-events/" e.Summary e.DtStart.AsDateTimeOffset e.Description e.DtEnd.AsDateTimeOffset calendarStream
+            for location, icon, e in sourceEvents do
+                use! icon = icon
+                for e in e do
+                    do! syncOneEvent location e.Name e.StartTime e.Description (if e.EndTime.HasValue then e.EndTime.GetValueOrDefault() else e.StartTime.AddHours 1.) icon
             for remainingDiscordEvent in existingDiscordEvents.Values do
-                if remainingDiscordEvent.StartTime > utcNowOffset then // Don't remove already started events
+                if remainingDiscordEvent.StartTime > now then // Don't remove already started events
                     do! remainingDiscordEvent.DeleteAsync()
-                    printfn $"Removed scheduled event '{remainingDiscordEvent.Name}' for '{guild}'."
+                    printfn $"Removed event '{remainingDiscordEvent.Name}' for '{guild}'."
         printfn "Processing finished."
         completion.TrySetResult() |> ignore
     })
