aef6ea762b331fe429336a77ff1513bcbe09d074
Hadrian Tang <hadrianwttang@outlook.com>
2023-01-14 12:37:17 +0800

    Fix space

--- Bot.fsx
+++ Bot.fsx
@@ -30,7 +30,7 @@ task {
         let maxEnd = now.AddYears(5).AddSeconds(-1.)
         // Discord API limitation: Don't add new already started events (including now) or start time >= 5 years into future (precise to seconds) or start time > end time (can equal)
         let filterEventByTime startTime endTime = now < startTime && startTime <= maxEnd && startTime <= endTime
-        printfn $"Initialized events. There are {calendarEvents.Count} F# calendar events ({calendarEvents |> Seq.filter (fun e -> filterEventByTime e.DtStart.AsDateTimeOffset e.DtEnd.AsDateTimeOffset) |> Seq.length} applicable)\
+        printfn $"Initialized events. There are {calendarEvents.Count} F# calendar events ({calendarEvents |> Seq.filter (fun e -> filterEventByTime e.DtStart.AsDateTimeOffset e.DtEnd.AsDateTimeOffset) |> Seq.length} applicable) \
                  and {sourceGuildEvents |> Seq.collect (fun (_, _, events) -> events) |> Seq.length} source guild events."
         for guild in client.Guilds do
             if Map.containsKey guild.Id sourceGuilds then () else // Ignore source guilds
